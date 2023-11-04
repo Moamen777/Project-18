1e1207027c101597cd43d8222af63ad8bf0611b8
Moamen777 <moamendev77@gmail.com>
2023-11-04 14:59:55 +0200

    Project 18

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+var userDegree = Number(window.prompt("enter your degree"))
+
+if(userDegree >=25){
+     window.alert("congrats you have passed")
+}
+else
+{
+    window.alert("unfortunatly you failed")
+}
